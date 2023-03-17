9a89d03a4ef1a2b250e73e20cf95565c2954fc68
byeongdolee <bdbdbdlee@gmail.com>
2023-03-17 21:52:53 +0900

    client ch6

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-
-#include <sys/socket.h>
-#include <cerrno>
-#include <cstdio>
-#include <cstdlib>
-#include <netinet/in.h>
-#include <cstring>
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
-#include <cassert>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
 
-const size_t k_max_msg = 4096;
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
 
 static void die(const char *msg) {
     int err = errno;
@@ -16,51 +20,49 @@ static void die(const char *msg) {
     abort();
 }
 
-static void msg(const char *msg) {
-    fprintf(stderr, "%s\n", msg);
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
 static int32_t write_all(int fd, const char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = write(fd, buf, n);
         if (rv <= 0) {
-            return -1;
+            return -1;  // error
         }
-
-        assert((size_t) rv <= n);
+        assert((size_t)rv <= n);
         n -= (size_t)rv;
         buf += rv;
     }
     return 0;
 }
 
-static int32_t read_full(int fd, char *buf, size_t n) {
-    while (n > 0) {
-        ssize_t rv = read(fd, buf, n);
-        if (rv <= 0) {
-            return -1;
-        }
+const size_t k_max_msg = 4096;
 
-        assert((size_t) rv <= n);
-        n -= (size_t)rv;
-        buf += rv;
-    }
-    return 0;
-}
-
-static int32_t query(int fd, const char *text) {
-    auto len = (uint32_t) strlen(text);
+// the `query` function was simply splited into `send_req` and `read_res`.
+static int32_t send_req(int fd, const char *text) {
+    uint32_t len = (uint32_t)strlen(text);
     if (len > k_max_msg) {
         return -1;
     }
 
     char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);
+    memcpy(wbuf, &len, 4);  // assume little endian
     memcpy(&wbuf[4], text, len);
-    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
-        return err;
-    }
+    return write_all(fd, wbuf, 4 + len);
+}
 
+static int32_t read_res(int fd) {
+    // 4 bytes header
     char rbuf[4 + k_max_msg + 1];
     errno = 0;
     int32_t err = read_full(fd, rbuf, 4);
@@ -73,22 +75,24 @@ static int32_t query(int fd, const char *text) {
         return err;
     }
 
-    memcpy(&len, rbuf, 4);
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
     if (len > k_max_msg) {
         msg("too long");
         return -1;
     }
 
+    // reply body
     err = read_full(fd, &rbuf[4], len);
     if (err) {
         msg("read() error");
         return err;
     }
 
-    rbuf[4 +len] = '\0';
+    // do something
+    rbuf[4 + len] = '\0';
     printf("server says: %s\n", &rbuf[4]);
     return 0;
-
 }
 
 int main() {
@@ -100,28 +104,28 @@ int main() {
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(8080);
-    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // 127.0.0.1
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
     int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) {
         die("connect");
     }
 
-    int32_t err = query(fd, "God bless you");
-    if (err) {
-        goto L_DONE;
+    // multiple pipelined requests
+    const char *query_list[3] = {"hello1", "hello2", "hello3"};
+    for (size_t i = 0; i < 3; ++i) {
+        int32_t err = send_req(fd, query_list[i]);
+        if (err) {
+            goto L_DONE;
+        }
+    }
+    for (size_t i = 0; i < 3; ++i) {
+        int32_t err = read_res(fd);
+        if (err) {
+            goto L_DONE;
+        }
     }
 
-    err = query(fd, "hello2");
-    if (err) {
-        goto L_DONE;
-    }
-
-    err = query(fd, "hello3");
-    if (err) {
-        goto L_DONE;
-    }
-
-L_DONE:
+    L_DONE:
     close(fd);
     return 0;
 }
